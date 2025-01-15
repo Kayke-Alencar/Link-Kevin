d8e3c570066f0d74fb368a86be8c13f9dea11ef7
Kayke-Alencar <kayke.silva15@etec.sp.gov.br>
2025-01-14 23:39:13 -0300

    arrumando link e criado layout para pc

--- javascript/Reload.js
+++ javascript/Reload.js
@@ -2,7 +2,8 @@ const whats = document.querySelector("#Whats");
 const insta =  document.querySelector("#Insta");
 
 whats.addEventListener("click", ()=>{
-    window.location.href = "https://abrir.link/TtlAK";
+    const msg = "Ola Kevin, vim  pelo link e Gostaria de saber mais informações."
+    window.location.href = "https://wa.me/5519992919115?text="+msg;
 });
 insta.addEventListener("click", ()=>{
     window.location.href = "https://www.instagram.com/dr.kevin_cremasco/"
