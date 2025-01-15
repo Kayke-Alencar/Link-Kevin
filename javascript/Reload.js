de923450f96cabbee730b0d36ed273627ca62bf9
Kayke-Alencar <kayke.silva15@etec.sp.gov.br>
2025-01-14 22:23:16 -0300

    Corrigindo ft e melhorando btn

---
+++ javascript/Reload.js
@@ -0,0 +1,9 @@
+const whats = document.querySelector("#Whats");
+const insta =  document.querySelector("#Insta");
+
+whats.addEventListener("click", ()=>{
+    window.location.href = "https://abrir.link/TtlAK";
+});
+insta.addEventListener("click", ()=>{
+    window.location.href = "https://www.instagram.com/dr.kevin_cremasco/"
+});
